f01cba949152c00bcc9ac763b2440478cadbbc06
hualinfeng123@126.com <hualinfeng123@126.com>
2024-09-28 01:14:40 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [data, setData] = React.useState('');
+  useEffect(() => {
+    fetch('https://finance.yiduoyunfan.asia/about')
+     .then(response => response.text())
+     .then(response => {
+       setData(response);
+      });
+
+  }, [data]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {data}
         </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
